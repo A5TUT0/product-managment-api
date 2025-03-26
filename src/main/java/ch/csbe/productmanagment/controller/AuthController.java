1d9199fe37db1d4bfd924146ad2cde4fa2f9d4d6
A5TUT0 <noahlezama2000@gmail.com>
2025-03-26 11:59:06 +0100

    🔐 Add JwtAuthFilter and integrate JWT validation into security configuration

--- src/main/java/ch/csbe/productmanagment/controller/AuthController.java
+++ src/main/java/ch/csbe/productmanagment/controller/AuthController.java
@@ -2,10 +2,15 @@ package ch.csbe.productmanagment.controller;
 
 import ch.csbe.productmanagment.dto.AuthRequest;
 import ch.csbe.productmanagment.dto.AuthResponse;
+import ch.csbe.productmanagment.model.User;
+import ch.csbe.productmanagment.repository.UserRepository;
 import ch.csbe.productmanagment.security.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,8 +19,23 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody AuthRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRoles(Set.of("USER"));
+        return ResponseEntity.ok(userRepository.save(user));
     }
 }
